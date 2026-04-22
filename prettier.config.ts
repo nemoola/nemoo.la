ca7e29b2bbdb03b51528bdbc8991c43ed7c9e998
nemoola <144327005+nemoola@users.noreply.github.com>
2026-04-22 20:27:35 +0900

    chore: Nuxt製のポートフォリオサイト一式を削除

--- prettier.config.ts
+++
@@ -1,8 +0,0 @@
-import { type Config } from "prettier";
-
-const config: Config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.ts",
-};
-
-export default config;
